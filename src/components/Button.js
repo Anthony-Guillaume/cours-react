5969159a3ebe3686a7551392aee1f12e1d9059e0
Anthony-Guillaume <aguillau2@gmail.com>
2020-11-02 16:28:09 +0100

    Add click Handler for Button class

--- src/components/Button.js
+++ src/components/Button.js
@@ -2,9 +2,13 @@ import React from 'react';
 
 export class Button extends React.Component
 {
+    handleClick = () => {
+        console.log("text");
+      }
+    
     render() {
         return (
-        <button onClick> Cliquez ici ! </button>
+            <button onClick={this.handleClick}>Cliquez ici</button>
         );
     }
 }
